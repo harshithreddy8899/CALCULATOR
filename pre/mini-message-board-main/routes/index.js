d4d5f208437cd1bc9debb96d0d92a4648f4b51d2
harshithreddy8899 <harshithreddy8@gmail.com>
2023-11-08 00:20:32 -0500

    final commit

--- pre/mini-message-board-main/routes/index.js
+++ pre/mini-message-board-main/routes/index.js
@@ -1,33 +1,9 @@
-var express = require('express');
-var router = express.Router();
-
-const messages = [
-  {
-    text: 'Is this thing on?',
-    user: 'spghtti',
-    added: new Date(),
-  },
-];
+const express = require("express");
+const router = express.Router();
 
 /* GET home page. */
-router.get('/', function (req, res, next) {
-  res.render('index', { title: 'Mini messageboard', messages: messages });
-});
-
-/* GET new form page. */
-
-router.get('/new', function (req, res, next) {
-  res.render('new', { title: 'Submit a post' });
-});
-
-router.post('/new', function (req, res, next) {
-  console.log();
-  messages.push({
-    text: req.body.messageText,
-    user: req.body.nameText,
-    added: new Date(),
-  });
-  res.redirect('/');
+router.get("/", function (req, res) {
+  res.redirect("/catalog");
 });
 
 module.exports = router;
